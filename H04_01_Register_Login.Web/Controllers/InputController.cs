15903f4cbc7a6e19ec5036d0483a1e6b7893724c
Brendy Verstreken <vbrendy@hotmail.com>
2020-03-26 10:50:24 +0100

    Add ViewModel

---
+++ H04_01_Register_Login.Web/Controllers/InputController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using H04_01_Register_Login.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H04_01_Register_Login.Web.Controllers
+{
+    public class InputController : Controller
+    {
+        public IActionResult Index()
+        {
+            InputVm model = new InputVm();
+            return View(model);
+        }
+    }
+}
---
+++ H04_01_Register_Login.Web/ViewModels/InputVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H04_01_Register_Login.Web.ViewModels
+{
+    public class InputVm
+    {
+        [Display (Name = "Naam")]
+        public string Naam { get; set; }
+
+        [Display(Name = "Voornaam")]
+        public string Voornaam { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
