04c588957a8f4eb09b07a1b9cfe473e9327937b3
Brendy Verstreken <vbrendy@hotmail.com>
2020-03-26 10:43:39 +0100

    Add project files.

---
+++ H04_01_Register_Login.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace H04_01_Register_Login.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
